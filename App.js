8316cca8bde83d5ecf805e058fba15c9d3b3acf2
viswa3047 <viswa3047@gmail.com>
2025-04-02 17:05:01 +0530

    errors ignore

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { LogBox } from "react-native";
 import { Provider } from 'react-redux';
 import Navigators from './src/navigators';
 import { Store } from './src/Store';
 
+LogBox.ignoreLogs(["Require cycle:"]);
 export default () => (
   <Provider store={Store}>
     <Navigators />
